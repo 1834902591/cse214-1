72cb71499d6c4891155053f170495277ed513279
Md Anik <65561883+1834902582@users.noreply.github.com>
2020-06-24 10:33:40 +0600

    Add files via upload

---
+++ lab7/3.c
@@ -0,0 +1,71 @@
+/*Define a "Size" function to find the size of the linked list.*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node node;
+struct Node{
+int value;
+node *next;
+};
+
+void print_list(node *temp);
+node *create_list();
+int size(node *temp);
+
+
+int main(){
+node *head=NULL;
+
+
+printf("\n");
+head=create_list(head);
+print_list(head);
+printf("SIZE=%d\n",size(head));
+
+return 0;
+}
+
+node *create_list(){
+
+node *a,*b,*c,*d;
+node *head;
+
+a=(node*)malloc(sizeof(node));
+b=(node*)malloc(sizeof(node));
+c=(node*)malloc(sizeof(node));
+d=(node*)malloc(sizeof(node));
+
+head=a;
+
+a->value=1;
+a->next=&b;
+b->value=2;
+b->next=&c;
+c->value=3;
+c->next=&d;
+d->value=4;
+d->next=NULL;
+
+return a;
+
+void print_list(node *temp){
+if(temp==NULL){
+    return 1;
+}
+while(temp!=NULL){
+    printf("%p\t %d\t %p\n", &temp, temp->value, temp->next);
+    temp=temp->next;
+}
+}
+
+int size(node *temp){
+ int count =0;
+ while (temp!=NULL){
+    count++;
+    temp=temp->next;
+ }
+return count;
+
+}
